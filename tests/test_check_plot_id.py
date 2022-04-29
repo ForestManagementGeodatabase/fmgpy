f3eb0e0085d1f4c3a2dc1b4c3541e89acd1c8701
aross26 <graniteBasalt84&>
2022-04-29 16:15:06 -0500

    update test_check_plot_id with relative paths

--- tests/test_check_plot_id.py
+++ tests/test_check_plot_id.py
@@ -1,2 +1,12 @@
-from fmgpy.check_plot_id import check_plot_id
+# from fmgpy.check_plot_id import check_plot_id
+import os
+import arcpy
+
+test_folder = relative_path = os.path.relpath("./data/Pool_17_20210930")
+
+test_folder_real = os.path.realpath(test_folder)
+
+age_path = os.path.join(test_folder_real, "Age.shp")
+
+age = arcpy.MakeFeatureLayer_management(age_path, "age")
 
